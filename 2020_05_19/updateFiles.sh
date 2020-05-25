02c8f447b6c8a46b383f2f2a7bcca0259feefcdd
Jake Lane <john.jake.lane@cern.ch>
2020-05-25 14:00:14 +0100

    update slides

--- 2020_05_19/updateFiles.sh
+++ 2020_05_19/updateFiles.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 cd antiSym_simple
 ./getFiles.sh antiSym_simple/2/
+mkdir 3
+cd 3
+
+./getFiles.sh antiSym_simple/3/
+
+cd ..
 cd ..
 
 cd antiSym_legendre
 ./getFiles.sh antiSym_legendre/2/
+mkdir 3
+cd 3
+
+./getFiles.sh antiSym_legendre/3/
+
+cd ..
 cd ..
 
 cd antiSym_chebyshev
@@ -14,4 +26,12 @@ cd ..
 
 cd legendre
 ./getFiles.sh legendre/2/
+mkdir 3
+cd 3
+
+./getFiles.sh legendre/3/
+
 cd ..
+cd ..
+
+
